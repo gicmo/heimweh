6cba00b89d1c37e904d44a68802230929c719488
Christian Kellner <christian@kellner.me>
2017-02-06 18:05:56 +0100

    [cfg] skeleton config file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate rustc_serialize;
 extern crate git2;
-
+extern crate toml;
 #[macro_use]
 extern crate clap;
 
+use std::io::prelude::*;
 use std::path::Path;
+use std::fs::File;
 
 use clap::{App, ArgMatches, SubCommand};
 
@@ -52,6 +54,27 @@ fn bootstrap(matches: &ArgMatches) -> Result<(), git2::Error> {
     Ok(())
 }
 
+
+#[derive(Debug, RustcDecodable)]
+struct Config {
+    castle: Vec<SourceEntry>,
+}
+
+#[derive(Debug, RustcDecodable)]
+struct SourceEntry {
+    url: String,
+}
+
+fn read_config(path: &str) -> Result<Config, String> {
+    let mut file = File::open(&path).map_err(|_| "Could not open file")?;
+    let mut data = String::new();
+
+    file.read_to_string(&mut data)
+        .map_err(|_| "Could not read file")?;
+
+    let cfg: Config = toml::decode_str(&data).ok_or_else(|| "Invalid config file")?;
+    Ok(cfg)
+}
 
 const MAIN_USAGE: &'static str = "
 -v, --verbose 'show what is going on'
